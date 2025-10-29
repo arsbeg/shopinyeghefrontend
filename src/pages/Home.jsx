8b617f87c8df47ef93b77aa5939611729fc93eee
Arsen WorkPC <arsbeg@mail.ru>
2025-10-29 17:33:22 +0400

    Slogan shadow design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <div>
       <HeroCarousel />
-      <h2 className="text-3xl text-center font-bold mb-4 text-gray-800 uppercase">Your City, Your Store — Yeghegnadzor Online!</h2>
+      <h2 className="text-3xl text-center text-shadow-lg/40 font-bold mb-4 text-gray-800 uppercase">Your City, Your Store — Yeghegnadzor Online!</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {stores.map((store) => (
           <div
