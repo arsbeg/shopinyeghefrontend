40561224119866ebddc9ff611e38d991e10295c8
Arsen Beglaryan <arsen.beglaryan@eum-armenia.eu>
2026-08-07 09:35:20 +0400

    testing text under chart

--- src/components/admin/StatisticsTab.jsx
+++ src/components/admin/StatisticsTab.jsx
@@ -59,6 +59,7 @@ export default function StatisticsTab() {
         </tbody>
       </table>
       <StoreRevenueChart data={stores} />
+      <h2 className="text-sm md:text-base lg:text-xl font-semibold mb-4">Test for chart</h2>
     </div>
   );
 }
